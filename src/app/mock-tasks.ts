023a1517433dc70e1a598510da7583ddac4ba544
Ryan Mui <ReachRyanMui@gmail.com>
2023-09-27 23:06:52 -0400

    20230927 Initial commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task'
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Interview',
+        day: 'Sept 28th at 3:00pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Dinner with Family',
+        day: 'Sept 25th at 6:30pm',
+        reminder: true,
+    }
+]
